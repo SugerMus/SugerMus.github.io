a9cd13afdf594e936fed93a5c16f6429ce651eaa
suger <2397100743@qq.com>
2025-05-08 15:50:49 +0800

    Site updated: 2025-05-08 15:50:49

--- js/anjianfangdou.js
+++ js/anjianfangdou.js
@@ -1,28 +1,7 @@
-// 防抖全局计时器
+// 防抖全局计时器 （未实现）
 let TT = null;    // time用来控制事件的触发
 // 防抖函数:fn->逻辑 time->防抖时间
 function debounce(fn, time) {
     if (TT !== null) clearTimeout(TT);
     TT = setTimeout(fn, time);
 }
-
-// 创建 Vue 实例并使用 Element UI
-new Vue({
-    el: 'body',
-    mounted() {
-        // 复制提醒
-        document.addEventListener("copy", () => {
-            debounce(() => {
-                this.$notify({
-                    title: "哎嘿！复制成功🍬",
-                    message: "若要转载最好保留原文链接哦，给你一个大大的赞！",
-                    position: 'top-left',
-                    offset: 50,
-                    showClose: true,
-                    type: "success",
-                    duration: 5000
-                });
-            }, 300);
-        });
-    }
-});
